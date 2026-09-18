71d65adf0266ab134f8c8acf44d36a2adb2d1657
Lucas <Lucas Arenda>
2026-09-18 17:43:03 -0300

    feat: início do projeto

---
+++ src/main/java/com/lucasarenda/achados_e_perdidos/AchadosEPerdidosApplication.java
@@ -0,0 +1,13 @@
+package com.lucasarenda.achados_e_perdidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchadosEPerdidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchadosEPerdidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasarenda/achados_e_perdidos/AchadosEPerdidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasarenda.achados_e_perdidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchadosEPerdidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
